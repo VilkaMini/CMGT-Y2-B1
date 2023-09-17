b7f33b049dc06c8a226fa00d881dde2a3bc0d125
Vilius <43584156+VilkaMini@users.noreply.github.com>
2023-09-17 15:21:05 +0200

    Teleportation to Painting Locations added

--- heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
+++ heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Painting : MonoBehaviour
 {
+    public GameObject player;
+
     public void EnterPainting()
     {
-        print("Entering painting" + gameObject.name);
+        if (gameObject.name == "Heaven1Painting")
+        {
+            player.transform.position = new Vector3(-20, player.transform.position.y, 15);
+        }
+        else if (gameObject.name == "Heaven2Painting")
+        {
+            player.transform.position = new Vector3(0, player.transform.position.y, 15);
+        }
+        else if (gameObject.name == "Heaven3Painting")
+        {
+            player.transform.position = new Vector3(20, player.transform.position.y, 15);
+        }
     }
 }
