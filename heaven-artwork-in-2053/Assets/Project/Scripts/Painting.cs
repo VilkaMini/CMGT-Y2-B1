8023b768fc47beb145c53be08487323fa8824e62
Vilius <alaunisvilius@gmail.com>
2023-09-18 13:25:38 +0200

    Added picture frames

--- heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
+++ heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
@@ -8,6 +8,7 @@ public class Painting : MonoBehaviour
 
     public void EnterPainting()
     {
+        print(gameObject.name);
         if (gameObject.name == "Heaven1Painting")
         {
             player.transform.position = new Vector3(-20, player.transform.position.y, 15);
