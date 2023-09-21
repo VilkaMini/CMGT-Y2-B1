c9cf09b98ac020094f9482b24113fc07ff7e49d5
Vilius <alaunisvilius@gmail.com>
2023-09-21 11:29:20 +0200

    Assets and settings updated

--- heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
+++ heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
@@ -9,15 +9,15 @@ public class Painting : MonoBehaviour
     public void EnterPainting()
     {
         print(gameObject.name);
-        if (gameObject.name == "Heaven1Painting")
+        if (gameObject.tag == "Heaven1Painting")
         {
             player.transform.position = new Vector3(-20, player.transform.position.y, 15);
         }
-        else if (gameObject.name == "Heaven2Painting")
+        else if (gameObject.tag == "Heaven2Painting")
         {
             player.transform.position = new Vector3(0, player.transform.position.y, 15);
         }
-        else if (gameObject.name == "Heaven3Painting")
+        else if (gameObject.tag == "Heaven3Painting")
         {
             player.transform.position = new Vector3(20, player.transform.position.y, 15);
         }
