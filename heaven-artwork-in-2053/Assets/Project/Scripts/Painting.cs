ad7e10daff1baa1160648997b5dffa16475c6e75
Vilius <43584156+VilkaMini@users.noreply.github.com>
2023-10-27 14:38:56 +0200

    Restructuring main scene, fixing UI of the museum

--- heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
+++ heaven-artwork-in-2053/Assets/Project/Scripts/Painting.cs
@@ -6,62 +6,61 @@ using UnityEngine;
 public class Painting : MonoBehaviour
 {
     // Initiate variables
+    public int levelID;
     public GameObject player;
     public List<GameObject> spawnPoints;
-    public Coroutine activeCoroutine;
-    public bool activated = false;
+    public List<string> messagesForLoading;
 
     public GameObject canvas;
-    public TextMeshProUGUI counter;
-    public bool teleportInterupcted;
+    public GameObject button;
+    public GameObject fancyText;
+    public TextMeshProUGUI fancyTextT;
 
     public void EnterPainting()
     {
-        if (!activated)
+        // Check the painting tag and teleport the player to correct heaven location
+        // Each heaven trigger produces updates to update the active heaven and turning the light in the museum to another light
+        if (gameObject.tag == "Heaven1Painting")
         {
-            activated = true;
-            teleportInterupcted = false;
-            // Check the painting tag and teleport the player to correct heaven location
-            // Each heaven trigger produces updatees to update the active heaven and turning the light in the museum to another light
-            if (gameObject.tag == "Heaven1Painting")
-            {
-                activeCoroutine = StartCoroutine(EnterLevel(0, 1));
-            }
-            else if (gameObject.tag == "Heaven2Painting")
-            {
-                activeCoroutine = StartCoroutine(EnterLevel(1, 2));
-            }
-            else if (gameObject.tag == "Heaven3Painting")
-            {
-                activeCoroutine = StartCoroutine(EnterLevel(2, 2));
-            }
+            TurnOnUI();
         }
+        else if (gameObject.tag == "Heaven2Painting")
+        {
+            TurnOnUI();
+        }
+        else if (gameObject.tag == "Heaven3Painting")
+        {
+            TurnOnUI();
+        }
+    }
+
+    public void EnterLevelClicked()
+    {
+        button.SetActive(false);
+        fancyText.SetActive(true);
+        StartCoroutine(EnterLevel(levelID, levelID+1));
+    }
+
+    void TurnOnUI()
+    {
+        canvas.SetActive(true);
+    }
+    public void TurnOffUI()
+    {
+        canvas.SetActive(false);
     }
 
     IEnumerator EnterLevel(int heavenID, int lightID)
     {
-        canvas.SetActive(true);
-        for (int i = 5; i > 0; i--)
+        for (int i = 0; i < 6; i++)
         {
-            counter.text = i.ToString();
             yield return new WaitForSeconds(1);
+            fancyTextT.text = messagesForLoading[i] + "...";
         }
-        if (!teleportInterupcted)
-        {
-            player.transform.position = spawnPoints[heavenID].transform.position;
-            Actions.OnEnterHeaven(heavenID);
-            Actions.TurnLightOn(lightID);
-        }
+        player.transform.position = spawnPoints[heavenID].transform.position;
+        Actions.OnEnterHeaven(heavenID);
+        Actions.TurnLightOn(lightID);
     }
 
-    public void CancelEntering()
-    {
-        if (activated) {
-            activated = false;
-            StopCoroutine(activeCoroutine);
-            canvas.SetActive(false);
-            teleportInterupcted = true;
-            activated = false;
-        }
-    }
+    
 }
